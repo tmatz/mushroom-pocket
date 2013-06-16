cd40a39c96511560a43f8740d2ab9ce1b49c3294
tmatz <t.matsuda.1000@gmail.com>
2013-06-17 02:25:18 +0900

    creanup

--- src/pocketmushroom/MushroomActivity.java
+++ src/pocketmushroom/MushroomActivity.java
@@ -10,27 +10,14 @@ import android.view.*;
 import android.widget.*;
 import org.tmatz.pocketmushroom.*;
 
-public class MushroomActivity extends FragmentActivity
-implements ListFragment.OnListItemClickListener
-, EntryDetailFragment.OnFieldSelectedListener
-, CustomFragment.OnDetachListener
+public class MushroomActivity
+	extends FragmentActivity
+	implements ListFragment.OnListItemClickListener
+	, EntryDetailFragment.OnFieldSelectedListener
+	, CustomFragment.OnDetachListener
 {
-
-	public void onDetach(CustomFragment f)
-	{
-		if (TAG_ENTRY_LIST.equals(f.getTag()))
-		{
-			mGroupId = -1;
-			mEntryId = -1;
-		}
-		else if (TAG_ENTRY_DETAILS.equals(f.getTag()))
-		{
-			mEntryId = -1;
-		}
-	}
-
-    public static final String ACTION_INTERCEPT = "com.adamrocker.android.simeji.ACTION_INTERCEPT";
-    public static final String EXTRA_REPLACE_KEY = "replace_key";
+	public static final String ACTION_INTERCEPT = "com.adamrocker.android.simeji.ACTION_INTERCEPT";
+	public static final String EXTRA_REPLACE_KEY = "replace_key";
 	public static final String STATE_GROUP_ID = "group_id";
 	public static final String STATE_ENTRY_ID = "entry_id";
 	private static final int REQUEST_LOGIN = 1;
@@ -48,22 +35,29 @@ implements ListFragment.OnListItemClickListener
 	@Override
 	public void onSaveInstanceState(Bundle outState)
 	{
+		mLogBuilder.setLength(0);
+		Log.i(TAG, mLogBuilder
+			.append("onSaveInstanceState group = ")
+			.append(mGroupId)
+			.append(", entry = ")
+			.append(mEntryId)
+			.toString());
+
 		super.onSaveInstanceState(outState);
 
 		outState.putInt(STATE_GROUP_ID, mGroupId);
 		outState.putInt(STATE_ENTRY_ID, mEntryId);
-
-		mLogBuilder.setLength(0);
-		Log.i(TAG, mLogBuilder
-			  .append("onSaveInstanceState group = ")
-			  .append(mGroupId)
-			  .append(", entry = ")
-			  .append(mEntryId)
-			  .toString());
 	}
 
 	private void restoreInstanceState(Bundle savedInstanceState)
 	{
+		mLogBuilder.setLength(0);
+		Log.i(TAG, mLogBuilder
+			.append("restoreInstanceState group = ")
+			.append(mGroupId)
+			.append(", entry = ")
+			.append(mEntryId).toString());
+
 		if (savedInstanceState != null)
 		{
 			mGroupId = savedInstanceState.getInt(STATE_GROUP_ID, -1);
@@ -75,32 +69,30 @@ implements ListFragment.OnListItemClickListener
 			mGroupId = pref.getInt(STATE_GROUP_ID, -1);
 			mEntryId = pref.getInt(STATE_ENTRY_ID, -1);
 		}
-
-		mLogBuilder.setLength(0);
-		Log.i(TAG, mLogBuilder
-			  .append("restoreInstanceState group = ")
-			  .append(mGroupId)
-			  .append(", entry = ")
-			  .append(mEntryId).toString());
 	}
 
-    @Override
-    public void onCreate(Bundle savedInstanceState)
+	@Override
+	public void onCreate(Bundle savedInstanceState)
 	{
-        super.onCreate(savedInstanceState);
-
+		mLogBuilder.setLength(0);
+		Log.i(TAG, mLogBuilder
+			.append("onCreate")
+			.append((savedInstanceState != null)? "with state" : "")
+			.toString());
+		
+		super.onCreate(savedInstanceState);
 		restoreInstanceState(savedInstanceState);
 
 		if (!PocketDatabase.isReadable())
 		{
-			Toast.makeText(this, R.string.cant_open_pocket, Toast.LENGTH_SHORT).show();
 			Log.e(TAG, "pocket database is unreadable");
+			Toast.makeText(this, R.string.cant_open_pocket, Toast.LENGTH_SHORT).show();
 			setResult(RESULT_CANCELED);
 			finish();
 			return;
 		}
 
-        mCallingPackage = getCallingPackage();
+		mCallingPackage = getCallingPackage();
 		if (mCallingPackage == null)
 		{
 			Log.e(TAG, "calling package unkown");
@@ -120,22 +112,20 @@ implements ListFragment.OnListItemClickListener
 		{
 			initFragment();
 		}
-
-		Log.i(TAG, "onCreate");
-    }
+   	 }
 
 	@Override
 	public void onStop()
 	{
+		Log.i(TAG, "onStop");
+
 		SharedPreferences pref = getPreferences(MODE_PRIVATE);
 		pref.edit()
 			.putInt(STATE_GROUP_ID, mGroupId)
 			.putInt(STATE_ENTRY_ID, mEntryId)
 			.commit();
-
+			
 		super.onStop();
-
-		Log.i(TAG, "onStop");
 	}
 
 	private void showLoginActivity()
@@ -151,8 +141,15 @@ implements ListFragment.OnListItemClickListener
 	@Override
 	public void onActivityResult(int request, int result, Intent data)
 	{
-		super.onActivityResult(request, result, data);
+		mLogBuilder.setLength(0);
+		Log.i(TAG, mLogBuilder
+			  .append("onActivityResult request = ")
+			  .append(request)
+			  .append(", result = ")
+			  .append(result)
+			  .toString());
 
+		super.onActivityResult(request, result, data);
 		switch (request)
 		{
 			case REQUEST_LOGIN:
@@ -172,35 +169,27 @@ implements ListFragment.OnListItemClickListener
 				}
 				break;
 		}
-
-		mLogBuilder.setLength(0);
-		Log.i(TAG, mLogBuilder
-			  .append("onActivityResult request = ")
-			  .append(request)
-			  .append(", result = ")
-			  .append(result)
-			  .toString());
 	}
 
 	private void initFragment()
 	{
-		addGroupListFragment();
+		Log.i(TAG, "initFragment");
 
+		addGroupListFragment();
 		if (mGroupId != -1)
 		{
 			replaceEntryListFragment();
 		}
-
 		if (mEntryId != -1)
 		{
 			replaceEntryDetailsFragment();
 		}
-
-		Log.i(TAG, "initFragment");
 	}
 
 	private void replaceEntryDetailsFragment()
 	{
+		Log.i(TAG, "replaceEntryDetailsFragment");
+
 		FragmentManager fm = getSupportFragmentManager();
 		EntryDetailFragment f = new EntryDetailFragment();
 		Bundle args = new Bundle();
@@ -211,12 +200,12 @@ implements ListFragment.OnListItemClickListener
 			.replace(R.id.fragment, f, TAG_ENTRY_DETAILS)
 			.addToBackStack(TAG_ENTRY_DETAILS)
 			.commit();
-
-		Log.i(TAG, "replaceEntryDetailsFragment");
 	}
 
 	private void replaceEntryListFragment()
 	{
+		Log.i(TAG, "replaceEntryListFragment");
+
 		FragmentManager fm = getSupportFragmentManager();
 		ListFragment f = new ListFragment();
 		Bundle args = new Bundle();
@@ -227,12 +216,12 @@ implements ListFragment.OnListItemClickListener
 			.replace(R.id.fragment, f, TAG_ENTRY_LIST)
 			.addToBackStack(TAG_ENTRY_LIST)
 			.commit();
-
-		Log.i(TAG, "replaceEntryListFragment");
 	}
 
 	private void addGroupListFragment()
 	{
+		Log.i(TAG, "addGroupListFragment");
+
 		FragmentManager fm = getSupportFragmentManager();
 		ListFragment f = new ListFragment();
 		Bundle args = new Bundle();
@@ -241,12 +230,37 @@ implements ListFragment.OnListItemClickListener
 		fm.beginTransaction()
 			.replace(R.id.fragment, f, TAG_GROUP_LIST)
 			.commit();
+	}
 
-		Log.i(TAG, "addGroupListFragment");
+	public void onDetach(CustomFragment f)
+	{
+		mLogBuilder.setLength(0);
+		Log.i(TAG, mLogBuilder
+			.append("onDetach tag = ")
+			.append(f.getTag())
+			.toString());
+
+		if (TAG_ENTRY_LIST.equals(f.getTag()))
+		{
+			mGroupId = -1;
+			mEntryId = -1;
+		}
+		else if (TAG_ENTRY_DETAILS.equals(f.getTag()))
+		{
+			mEntryId = -1;
+		}
 	}
 
 	public void onListItemSelected(ListFragment f, ListFragment.ItemData data)
 	{
+		mLogBuilder.setLength(0);
+		Log.i(TAG, mLogBuilder
+			.append("onListItemSelected tag = ")
+			.append(f.getTag())
+			.append(", id = ")
+			.append(data.id)
+			.toString());
+
 		if (TAG_GROUP_LIST.equals(f.getTag()))
 		{
 			mGroupId = data.id;
@@ -257,21 +271,13 @@ implements ListFragment.OnListItemClickListener
 			mEntryId = data.id;
 			replaceEntryDetailsFragment();
 		}
-		
-		mLogBuilder.setLength(0);
-		Log.i(TAG, mLogBuilder
-			.append("onListItemSelected tag = ")
-			.append(f.getTag())
-			.append(", id = ")
-			.append(data.id)
-			.toString());
 	}
 
 	public void onFieldSelected(EntryDetailFragment f, String value)
 	{
-		replace(value);
-		
 		Log.i(TAG, "onFieldSelected");
+
+		replace(value);
 	}
 
 	// long press return button. finish app.
@@ -285,14 +291,14 @@ implements ListFragment.OnListItemClickListener
 		return super.onKeyLongPress(code, event);
 	}
 
-    // send result string to calling package.
-    private void replace(String result)
+	// send back result string to calling package.
+	private void replace(String result)
 	{
-        Intent data = new Intent();
-        data.putExtra(EXTRA_REPLACE_KEY, result);
-        setResult(RESULT_OK, data);
-        finish();
-		
 		Log.i(TAG, "replace");
-    }
+
+		Intent data = new Intent();
+		data.putExtra(EXTRA_REPLACE_KEY, result);
+		setResult(RESULT_OK, data);
+		finish();
+	}
 }
