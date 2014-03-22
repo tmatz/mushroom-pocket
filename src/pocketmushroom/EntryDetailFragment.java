eea80236b8eac988db47855184b35656a587458c
tmatz <t.matsuda.1000@gmail.com>
2014-03-22 19:12:40 +0900

    field list contains notes.

--- src/pocketmushroom/EntryDetailFragment.java
+++ src/pocketmushroom/EntryDetailFragment.java
@@ -1,15 +1,26 @@
 package pocketmushroom;
 
-import android.content.*;
-import android.database.*;
-import android.database.sqlite.*;
-import android.os.*;
-import android.text.method.*;
-import android.view.*;
-import android.view.View.*;
-import android.widget.*;
+import android.content.Context;
+import android.database.Cursor;
+import android.database.MatrixCursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.os.Bundle;
+import android.text.method.HideReturnsTransformationMethod;
+import android.text.method.PasswordTransformationMethod;
+import android.text.method.TransformationMethod;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.AdapterView;
 import android.widget.AdapterView.OnItemClickListener;
-import org.tmatz.pocketmushroom.*;
+import android.widget.ListView;
+import android.widget.SimpleCursorAdapter;
+import android.widget.TextView;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.List;
+import org.tmatz.pocketmushroom.R;
 
 public class EntryDetailFragment extends CustomFragment
 {
@@ -22,6 +33,21 @@ public class EntryDetailFragment extends CustomFragment
 	{
 		void onFieldSelected(EntryDetailFragment f, String value);
 	}
+	
+	class RowData
+	{
+		public String label;
+		public String value;
+		public int hide;
+				
+		public void Add(MatrixCursor.RowBuilder builder)
+		{
+			builder.add(0);
+			builder.add(label);
+			builder.add(value);
+			builder.add(hide);
+		}
+	}
 
 	private SQLiteDatabase mDatabase;
 	private PocketLock mPocketLock;
@@ -29,15 +55,7 @@ public class EntryDetailFragment extends CustomFragment
 	private int mEntryId;
 	private TextView mTitle;
 	private ListView mListView;
-	private TextView mNotes;
-	private final OnClickListener mOnClickListener = new OnClickListener()
-	{
-		public void onClick(View view)
-		{
-			String value = (String) view.getTag();
-			onFieldClick(value);
-		}
-	};
+
 	private final OnItemClickListener mOnItemClickListener = new OnItemClickListener()
 	{
 		public void onItemClick(AdapterView<?> parent, View view, int position, long id)
@@ -57,8 +75,6 @@ public class EntryDetailFragment extends CustomFragment
 		mTitle = (TextView) v.findViewById(R.id.title);
 		mListView = (ListView) v.findViewById(R.id.field_list);
 		mListView.setOnItemClickListener(mOnItemClickListener);
-		mNotes = (TextView) v.findViewById(R.id.notes);
-		mNotes.setOnClickListener(mOnClickListener);
 		mPackageName = args.getString(ARG_PACKAGE_NAME);
 		mEntryId = args.getInt(ARG_ENTRY_ID, -1);
 		if (mPackageName != null && mEntryId != -1)
@@ -66,14 +82,11 @@ public class EntryDetailFragment extends CustomFragment
 			mDatabase = PocketDatabase.openDatabase();
 			mPocketLock = PocketLock.getPocketLock(mPackageName);
 			showFieldList();
-			Cursor c = mDatabase.rawQuery("select _id, title, notes from entries where _id = " + mEntryId, null);
+			Cursor c = mDatabase.rawQuery("select _id, title from entries where _id = " + mEntryId, null);
 			if (c.getCount() == 1 && c.moveToFirst())
 			{
 				String title = mPocketLock.decrypt(c.getString(1));
 				mTitle.setText(title);
-				String note = mPocketLock.decrypt(c.getString(2));
-				mNotes.setText(note);
-				mNotes.setTag(note);
 			}
 			c.close();
 		}
@@ -82,12 +95,69 @@ public class EntryDetailFragment extends CustomFragment
 
 	private void showFieldList()
 	{
-		Cursor c = mDatabase.rawQuery("select _id, title, value, is_hidden from fields where entry_id = " + mEntryId, null);
-		FilterCursor sc = new FilterCursor(c, "title", new DecryptColumnTransformer(mPocketLock));
+		List<RowData> rowDatas = new ArrayList<RowData>();
+		
+		{
+			Cursor c = mDatabase.rawQuery("select _id, title, value, is_hidden from fields where entry_id = " + mEntryId, null);
+			while (c.moveToNext())
+			{
+				RowData data = new RowData();
+				data.label = mPocketLock.decrypt(c.getString(1));
+				data.value = mPocketLock.decrypt(c.getString(2));
+				data.hide = c.getInt(3);
+
+				if (data.value != null && !data.value.isEmpty())
+
+				{
+					rowDatas.add(data);
+				}
+			}
+			c.close();
+		}
+
+		Collections.sort(rowDatas, new Comparator<RowData>()
+		{
+			@Override
+			public int compare(EntryDetailFragment.RowData p1, EntryDetailFragment.RowData p2)
+			{
+				return p1.label.compareTo(p2.label);
+			}
+		});
+		
+		{
+			Cursor c = mDatabase.rawQuery("select _id, notes from entries where _id = " + mEntryId, null);
+			if (c.moveToFirst())
+			{
+				RowData data = new RowData();
+				data.label = getResources().getString(R.string.notes);
+				data.value = mPocketLock.decrypt(c.getString(1));
+				data.hide = 0;
+
+				if (data.value != null && !data.value.isEmpty())
+				{
+					rowDatas.add(data);
+				}
+			}
+			c.close();
+		}
+
+		MatrixCursor cursor = new MatrixCursor(
+			new String[] {
+				PocketDatabase.COL_ID,
+				PocketDatabase.COL_TITLE,
+				PocketDatabase.COL_VALUE,
+				PocketDatabase.COL_IS_HIDDEN,
+			});
+
+		for (RowData data: rowDatas)
+		{
+			data.Add(cursor.newRow());
+		}
+				
 		SimpleCursorAdapter listAdapter = new CustomCursorAdapter(
 			getActivity(),
 			R.layout.field_list_item,
-			sc,
+			cursor,
 			new String[] {PocketDatabase.COL_TITLE, PocketDatabase.COL_VALUE},
 			new int[] {R.id.title, R.id.value});
 		mListView.setAdapter(listAdapter);
@@ -96,9 +166,12 @@ public class EntryDetailFragment extends CustomFragment
 
 	private class CustomCursorAdapter extends SimpleCursorAdapter
 	{
+		private int mColHidden;
+		
 		public CustomCursorAdapter(Context context, int layout, Cursor c, String[] from, int[] to)
 		{
 			super(context, layout, c, from, to);
+			mColHidden = c.getColumnIndex(PocketDatabase.COL_IS_HIDDEN);
 		}
 
 		@Override
@@ -106,7 +179,7 @@ public class EntryDetailFragment extends CustomFragment
 		{
 			super.bindView(view, context, cursor);
 			TextView value = (TextView) view.findViewById(R.id.value);
-			if (cursor.getInt(3) == 0)
+			if (cursor.getInt(mColHidden) == 0)
 			{
 				value.setTransformationMethod(mNormalTransformationMethod);
 			}
